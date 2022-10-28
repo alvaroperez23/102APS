619c6d7d15580cbdee11371ca026a78545acf88b
peresito23 <alvaroperezsanchez23@gmail.com>
2022-10-28 13:41:46 +0200

    o

--- tareaContinua1.js
+++
@@ -1,9 +0,0 @@
-//Apartado 1
-    console.log("\nAPARTADO 1\n");
-    let nombre1 = "Maxwell Wright"; let numero1 = "(0191) 719 6495"; let correo1 = "Curabitur.egestas.nunc@nonummyac.co.uk";
-    let nombre2 = "Raja Villarreal"; let numero2 = "0866 398 2895"; let correo2 = "posuere.vulputate@sed.com";
-    let nombre3 = "Helen Richards"; let numero3 = "0800 1111"; let correo3 = "libero@convallis.edu";
-
-    console.log("Nombre: " + nombre1 + " / Numero: " + numero1 + " / Correo: " + correo1);
-    // console.log("Nombre: " + nombre2 + " / Numero: " + numero2 + " / Correo: " + correo2);
-    console.log("Nombre: " + nombre3 + " / Numero: " + numero3 + " / Correo: " + correo3);
