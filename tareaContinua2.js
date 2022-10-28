ecf906b5f6025eb5994409e9cffcf2cae5eddc6b
peresito23 <alvaroperezsanchez23@gmail.com>
2022-10-28 13:48:33 +0200

    3

---
+++ tareaContinua2.js
@@ -0,0 +1,28 @@
+//Apartado 2
+console.log("\nAPARTADO 2\n");
+let objeto1 = new Object();
+objeto1.nombre = 'Maxwell Wright';
+objeto1.numero = '(0191) 719 6495';
+objeto1.correo = 'Curabitur.egestas.nunc@nonummyac.co.uk';
+
+let objeto2 = new Object();
+objeto2.nombre = 'Raja Villarreal';
+objeto2.numero = '0866 398 2895';
+objeto2.correo = 'posuere.vulputate@sed.com';
+
+let objeto3 = new Object();
+objeto3.nombre = 'Helen Richards';
+objeto3.numero = '0800 1111';
+objeto3.correo = 'libero@convallis.edu';
+
+let arrayDatos = [objeto1, objeto2, objeto3];
+
+let objeto4 = new Object();
+objeto4.nombre = 'Maisie Haley';
+objeto4.numero = '0913 531 3030 ';
+objeto4.correo = 'risus.Quisque@urna.ca.';
+
+
+arrayDatos.push(objeto4);
+console.log(arrayDatos);
+console.log(arrayDatos[0], arrayDatos[(arrayDatos.length) - 1]);
